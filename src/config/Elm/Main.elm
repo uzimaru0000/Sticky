b012a16bba820fe7b8e3ebfd23e7e8c6175527d0
ShujiOba <shuji365630@gmail.com>
2018-02-01 13:45:49 +0900

    config画面のelmファイルを生成

--- src/config/Elm/Main.elm
+++ src/config/Elm/Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Color exposing (..)
 
 
-main : Html msg
+type Msg
+    = NoOp
+
+
+type alias Model =
+    { bgColor : Color
+    , fontSize : Float
+    }
+
+
+main : Program Never Model Msg
 main =
-    text "hoge"
+    program
+        { init = init ! []
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
+
+
+init : Model
+init =
+    Model (rgb 238 238 238) 24
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    model ! []
+
+
+view : Model -> Html Msg
+view model =
+    text ""
